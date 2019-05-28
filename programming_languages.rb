0708927130495eda52c8ed5e2f76a022a9e99cb9
Alexander Riccio <test35965@gmail.com>
2019-05-28 21:06:15 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,10 +4,11 @@ def reformat_languages(languages)
   new_hash = {}
   languages.each do |language_type, languages_of_that_type|
     languages_of_that_type.each do |language, language_data|
-      binding.pry
       if new_hash.include?(language)
+        binding.pry
         new_hash[language][:style] << language_data[:type]
       else
+        binding.pry
         new_hash[language] = { :type => language_data[:type], :style => [language_type] }
       end
     end
