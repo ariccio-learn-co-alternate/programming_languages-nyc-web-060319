325955444e11862f49ad622242329249eeafd55b
Alexander Riccio <test35965@gmail.com>
2019-05-28 21:01:05 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,7 +2,8 @@ def reformat_languages(languages)
   new_hash = {}
   languages.each do |language_type, languages_of_that_type|
     languages_of_that_type.each do |language, language_data|
-      new_hash
+      binding.pry
+      #if new_hash.include?(language)
     end
   end
 end
